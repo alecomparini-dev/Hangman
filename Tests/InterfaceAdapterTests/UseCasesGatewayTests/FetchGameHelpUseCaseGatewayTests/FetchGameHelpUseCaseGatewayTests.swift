4c4bed4d08d167756532f7f353159e19e782ce53
Comparini <alecomparini.dev@gmail.com>
2024-01-23 11:01:54 -0300

    pre configurations tests ok

---
+++ Tests/InterfaceAdapterTests/UseCasesGatewayTests/FetchGameHelpUseCaseGatewayTests/FetchGameHelpUseCaseGatewayTests.swift
@@ -0,0 +1,13 @@
+//  Created by Alessandro Comparini on 23/01/24.
+//
+
+import XCTest
+
+class FetchGameHelpUseCaseGatewayTests: XCTestCase {
+
+    func test_() {
+        XCTAssertTrue(true)
+    }
+
+
+}
---
+++ Tests/InterfaceAdapterTests/UseCasesGatewayTests/SaveGameHelpUseCaseGatewayTests/SaveGameHelpUseCaseGatewayTests.swift
@@ -0,0 +1,21 @@
+//  Created by Alessandro Comparini on 23/01/24.
+//
+
+import XCTest
+
+final class SaveGameHelpUseCaseGatewayTests: XCTestCase {
+
+    override func setUpWithError() throws {
+
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+
+    func testExample() throws {
+        
+    }
+
+
+}
